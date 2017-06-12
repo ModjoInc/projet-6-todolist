d038d5d41fb2ee9903a4fe4cd30b4805a4b0a5c4
ModjoInc <habibem@gmail.com>
2017-06-12 11:36:46 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>TO DO LIST</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header></header>
+      <section>
+
+
+      <div class="todo">
+        <h2>A FAIRE</h2>
+        <form class="" action="" method="post">
+          <label><input type="checkbox" name="checkB" id="">tache1</label>
+
+
+        </form>
+        <button type="button" name="enregistrer">ENREGISTRER</button>
+      </div>
+      <div class="archive">
+        <h2>ARCHIVES</h2>
+        <div class="archList">
+
+        </div>
+      </div>
+       <form class="ajoutTache" action="" method="post">
+
+         <label for="tache">Ajouter une Tâche</label><input type="text" name="tache" id="tache">
+         <button type="button" name="ajout">Ajouter</button>
+       </form>
+
+      </section>
+    <footer></footer>
+
+
+    <script src="lib/app.js"></script>
+  </body>
+</html>
